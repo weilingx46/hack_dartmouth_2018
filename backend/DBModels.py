32f9c823a5b1cd9712be720892bebabc22391be3
Edward <edwardcdy@gmail.com>
2018-04-14 19:18:12 -0400

    Added base models

--- backend/DBModels.py
+++ backend/DBModels.py
@@ -1,4 +1,6 @@
 from mongoengine import *
+import secrets
+import time
 
 class Trip(EmbeddedDocument):
   memberStatuses = ListField()
@@ -12,9 +14,44 @@ class Friend(EmbeddedDocument):
 class User(Document):
   uName = StringField(max_length=30)
   uId = StringField(max_length=20)
-  uPassword = StringField(max_length=30)
+
   trippees = ListField(EmbeddedDocumentField(Friend))
   trips = ListField(EmbeddedDocumentField(Trip))
+
+  lastLogin = LongField()
+  authToken = StringField(max_length=20)
+
+
+def generateAccessToken():
+  secret = secrets.token_hex(nbytes=16)
+
+def login(uId,uName):
+  now = time.time()
+
+  results = User.objects(uId=uId)
+
+  if not results:
+    newUser = User(uId)
+    newUser.lastLogin = time.time()
+  else:
+    user = results[0]
+    print(user)
+
+
+# check auth token - if over time limit, delete
+# current auth token and force re-sign in
+def validateAuthToken(uId, authToken):
+  
+  results = User.objects(uId=uId)
+
+  if not results:
+    return None
+
+  user = results[0]
+  print(user)
+
+  now = time.time()
+
 
 
 
