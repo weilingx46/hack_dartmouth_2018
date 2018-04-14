43439f79999b4160088f88190d167750c3cc87e0
Weiling Huang <weiling.j.huang.19@dartmouth.edu>
2018-04-14 16:17:10 -0400

    "handler":

--- backend/mysite/mysite/urls.py
+++ backend/mysite/mysite/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   #Examples
+   #url(r'^$', 'myproject.view.home', name = 'home'),
+   #url(r'^blog/', include('blog.urls')),
+
+   url(r'^admin', admin.site.urls),
 ]
